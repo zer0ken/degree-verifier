5b4c927c503ed0cd66c0c1d63a5cb24bdb30cb2f
zer0ken <a37592777@gmail.com>
2024-06-20 15:46:47 +0900

    save

---
+++ src/main/java/org/konkuk/degreeverifier/editorframe/EditorFrame.java
@@ -0,0 +1,28 @@
+package org.konkuk.degreeverifier.editorframe;
+
+import org.konkuk.degreeverifier.common.components.DegreeVerifierFrame;
+import org.konkuk.degreeverifier.editorframe.components.SplitPanel;
+import org.konkuk.degreeverifier.editorframe.components.applypanel.ApplyPanel;
+
+import java.awt.*;
+
+import static org.konkuk.degreeverifier.ui.Dimensions.MINIMUM_EDIT_FRAME_SIZE;
+import static org.konkuk.degreeverifier.ui.Dimensions.PREFERRED_EDIT_FRAME_SIZE;
+import static org.konkuk.degreeverifier.ui.Strings.EDIT_FRAME_TITLE;
+
+public class EditorFrame extends DegreeVerifierFrame {
+    public EditorFrame() {
+        super();
+
+        setTitle(EDIT_FRAME_TITLE);
+        setMinimumSize(MINIMUM_EDIT_FRAME_SIZE);
+        setPreferredSize(PREFERRED_EDIT_FRAME_SIZE);
+        setSize(PREFERRED_EDIT_FRAME_SIZE);
+        setLayout(new BorderLayout());
+
+        add(new SplitPanel());
+        add(new ApplyPanel(), BorderLayout.SOUTH);
+
+        setVisible(true);
+    }
+}
