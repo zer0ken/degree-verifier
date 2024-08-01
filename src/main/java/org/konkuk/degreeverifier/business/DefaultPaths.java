a16056fcb93869d9fce7cb1e2b195ccd62fa79aa
zer0ken <a37592777@gmail.com>
2024-08-01 22:49:39 +0900

    디버그

--- src/main/java/org/konkuk/degreeverifier/business/DefaultPaths.java
+++ src/main/java/org/konkuk/degreeverifier/business/DefaultPaths.java
@@ -1,10 +1,25 @@
 package org.konkuk.degreeverifier.business;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 public class DefaultPaths {
-    public static final String ROOT_PATH =
-            new File(DefaultPaths.class.getProtectionDomain().getCodeSource().getLocation().getFile()).getParent();
+    public static final String ROOT_PATH;
+
+    static {
+        try {
+            ROOT_PATH = new File(
+                    URLDecoder.decode(
+                            DefaultPaths.class.getProtectionDomain().getCodeSource().getLocation().getFile(),
+                            "UTF-8"
+                    )
+            ).getParent();
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static final String VERIFIERS_PATH = ROOT_PATH + "/검사 기준/";
     public static final String TRANSCRIPT_PATH = ROOT_PATH + "/성적표/";
     public static final String COMMIT_PATH = ROOT_PATH + "/이수 학위/";
